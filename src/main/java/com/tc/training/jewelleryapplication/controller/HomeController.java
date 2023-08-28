5dee33907c955bfc05f26d82262389d03ac15c64
Priyanshu Gupta <priyanshugupta212@gmail.com>
2023-08-28 16:56:35 +0530

    added home controller

--- src/main/java/com/tc/training/jewelleryapplication/controller/HomeController.java
+++ src/main/java/com/tc/training/jewelleryapplication/controller/HomeController.java
@@ -4,9 +4,11 @@ import com.tc.training.jewelleryapplication.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class HomeController {
 
     @GetMapping("/")
