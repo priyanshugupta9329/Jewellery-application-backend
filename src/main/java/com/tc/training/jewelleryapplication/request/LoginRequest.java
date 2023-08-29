5fb4274e0a1cd3ff4efc07ab2a6e2b8436a8c38c
anshul <anshulprajapati990@gmail.com>
2023-08-29 14:53:34 +0530

    Edited validation

--- src/main/java/com/tc/training/jewelleryapplication/request/LoginRequest.java
+++ src/main/java/com/tc/training/jewelleryapplication/request/LoginRequest.java
@@ -1,5 +1,8 @@
 package com.tc.training.jewelleryapplication.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +12,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginRequest {
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Pattern(
+            regexp = ".*@gmail\\.com$",
+            message = "Email must be a Gmail address (ending with @gmail.com)"
+    )
     private String email;
+    @NotBlank(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{5,}$",
+            message = "Wrong Password!! Please Enter Valid Password"
+    )
     private String password;
 
 }
