5035bfc6139d4d6d4b09d794d7db65a68c090682
Priyanshu Gupta <priyanshugupta212@gmail.com>
2023-09-02 00:16:59 +0530

    resolved error

--- src/main/java/com/tc/training/jewelleryapplication/controller/PaymentController.java
+++ src/main/java/com/tc/training/jewelleryapplication/controller/PaymentController.java
@@ -63,7 +63,7 @@ public class PaymentController {
             notify.put("email",true);
             paymentLinkRequest.put("notify",notify);
 
-            paymentLinkRequest.put("callback_url","http://localhost:3000/payments"+orderId);
+            paymentLinkRequest.put("callback_url","http://localhost:3000/payment/"+orderId);
 
             paymentLinkRequest.put("callback_method","get");
 
