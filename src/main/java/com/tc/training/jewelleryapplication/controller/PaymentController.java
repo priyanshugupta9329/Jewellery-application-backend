3051d696c673b10bebb8d3738208ee33f2b79d36
anshul <anshulprajapati990@gmail.com>
2023-09-01 14:23:30 +0530

    Modification in PaymentController

--- src/main/java/com/tc/training/jewelleryapplication/controller/PaymentController.java
+++ src/main/java/com/tc/training/jewelleryapplication/controller/PaymentController.java
@@ -25,10 +25,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class PaymentController {
 
-    @Value("{razorpay.api.key}")
+    @Value("${razorpay.api.key}")
     String  apiKey;
 
-    @Value("{razorpay.api.secret}")
+    @Value("$ {razorpay.api.secret}")
     String  apiSecret;
 
     @Autowired
@@ -86,6 +86,7 @@ public class PaymentController {
 
     }
 
+    @GetMapping("/payments")
     public ResponseEntity<ApiResponse> redirect(@RequestParam(name="payment_id") String paymentId,@RequestParam(name="order_id")Long orderId) throws OrderException, RazorpayException {
 
         Order order=orderService.findOrderById(orderId);
